3182cd85168da3a45b1c437ef762728eb28e4523
Conor Haining <conor.haining@gmail.com>
2019-02-07 13:40:23 +0000

    Allow for specifying time changes on the departures and arrival boards

--- index.js
+++ index.js
@@ -13,16 +13,33 @@ const scheduleQuerying = require('./scheduleQuerying.js')(client);
 /*
  * Station
  */
+app.get(
+  ['/station/:stationCode/departures/:year/:month/:day/:time',
+   '/station/:stationCode/departures/']
+  , (req, res) => {
 
-app.get('/station/:stationCode/departures', (req, res) => {
+    let time;
+    if(req.params.time === undefined) {
+      time = DateTime.local().setLocale('en-GB');
+      
+    } else {
+      time = DateTime.fromObject({
+        year: parseInt(req.params.year),
+        month: parseInt(req.params.month),
+        day: parseInt(req.params.day),
+        hour: parseInt(req.params.time.substring(0, 2)),
+        minute: parseInt(req.params.time.substring(2, 4)),
+        second: 00
+      }).setLocale('en-GB');
+    }
+  
+    let later = time.plus({hours: 2});
 
-  let now = DateTime.local().setLocale('en-GB');
-  let later = now.plus({hours: 2});
 
   scheduleQuerying.getTiplocByCRS(req.params.stationCode)
     .catch((err) => {res.status(404).send(err);})
 
-    .then(tiploc => scheduleQuerying.getValidSchedulesFromLocation(tiploc, 'public_departure', now, later))
+    .then(tiploc => scheduleQuerying.getValidSchedulesFromLocation(tiploc, 'public_departure', time, later))
     .catch((err) => {res.status(500).send(err);})
 
     .then(schedules => scheduleFormatting.formatBoard(schedules, {board: "departure", tiploc: tiploc}))
@@ -31,15 +48,32 @@ app.get('/station/:stationCode/departures', (req, res) => {
 
 });
 
-app.get('/station/:stationCode/arrivals', (req, res) => {
+app.get(
+  ['/station/:stationCode/arrivals/:year/:month/:day/:time',
+   '/station/:stationCode/arrivals/']
+  , (req, res) => {
 
-  let now = DateTime.local().setLocale('en-GB');
-  let later = now.plus({hours: 2});
+  let time;
+  if(req.params.time === undefined) {
+    time = DateTime.local().setLocale('en-GB');
+    
+  } else {
+    time = DateTime.fromObject({
+      year: parseInt(req.params.year),
+      month: parseInt(req.params.month),
+      day: parseInt(req.params.day),
+      hour: parseInt(req.params.time.substring(0, 2)),
+      minute: parseInt(req.params.time.substring(2, 4)),
+      second: 00
+    }).setLocale('en-GB');
+  }
+
+  let later = time.plus({hours: 2});
 
   scheduleQuerying.getTiplocByCRS(req.params.stationCode)
     .catch((err) => {res.status(404).send(err);})
 
-    .then(tiploc => scheduleQuerying.getValidSchedulesFromLocation(tiploc, 'public_arrival', now, later))
+    .then(tiploc => scheduleQuerying.getValidSchedulesFromLocation(tiploc, 'public_arrival', time, later))
     .catch((err) => {res.status(500).send(err);})
 
     .then(schedules => scheduleFormatting.formatBoard(schedules, {board: 'arrival', tiploc: tiploc}))
