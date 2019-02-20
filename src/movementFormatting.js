83cfea064914527f9b835e5ff27d005e3f2fdba5
Conor Haining <conor.haining@gmail.com>
2019-02-20 20:08:49 +0000

    Code & Tests to predict timetable variations

--- src/movementFormatting.js
+++ src/movementFormatting.js
@@ -1,6 +1,23 @@
 module.exports = {
     performHeuristics: (schedule) => {
-        
+        let locationRecords = schedule['location_records'];
+        let previousVariation = undefined;
+
+        locationRecords = locationRecords.map(record => {
+            let timetableVariation = record['timetable_variation'];
+            
+            if (timetableVariation === undefined && previousVariation !== undefined){
+                record['timetable_variation_prediction'] = previousVariation;
+            } else {
+                previousVariation = timetableVariation;
+            }
+            
+            return record;
+        });
+
+        schedule['location_records'] = locationRecords;
+
+        return schedule;
     },
 
     createBasicMovementData: (schedule) => {
--- test/movementFormatting.test.js
+++ test/movementFormatting.test.js
@@ -4,16 +4,135 @@ describe('Movements', function() {
 
     describe('Heuristics', function() {
 
+        const movementFormatting = require('../src/movementFormatting.js');
+
         it('should infer an on time schedule', () =>{
-            expect(true).to.be.false;
+            let schedule = {
+                'location_records': [
+                    {
+                        'timetable_variation': 0
+                    },
+                    {
+                        'timetable_variation': 0
+                    },
+                    {
+                        'timetable_variation': 0
+                    },
+                    { },
+                    { }
+                ]
+            }
+
+            schedule = movementFormatting.performHeuristics(schedule);
+
+            schedule['location_records'].forEach((record) => {
+                expect(record).to.have.any.keys('timetable_variation', 'timetable_variation_prediction');
+                expect(record['timetable_variation_prediction']).to.satisfy((x) => {
+                    return x === undefined || x === 0;
+                });
+            });
+
         });
 
         it('should infer an early schedule', () =>{
-            expect(true).to.be.false;
+            let schedule = {
+                'location_records': [
+                    {
+                        'timetable_variation': 0
+                    },
+                    {
+                        'timetable_variation': 0
+                    },
+                    {
+                        'timetable_variation': -1
+                    },
+                    { },
+                    { }
+                ]
+            }
+
+            schedule = movementFormatting.performHeuristics(schedule);
+
+            schedule['location_records'].forEach((record) => {
+                expect(record).to.have.any.keys('timetable_variation', 'timetable_variation_prediction');
+                expect(record['timetable_variation_prediction']).to.satisfy((x) => {
+                    return x === undefined || x < 0;
+                });
+            });
         });
 
         it('should infer a late schedule', () =>{
-            expect(true).to.be.false;
+            let schedule = {
+                'location_records': [
+                    {
+                        'timetable_variation': 0
+                    },
+                    {
+                        'timetable_variation': 0
+                    },
+                    {
+                        'timetable_variation': 1
+                    },
+                    { },
+                    { }
+                ]
+            }
+
+            schedule = movementFormatting.performHeuristics(schedule);
+
+            schedule['location_records'].forEach((record) => {
+                expect(record).to.have.any.keys('timetable_variation', 'timetable_variation_prediction');
+                expect(record['timetable_variation_prediction']).to.satisfy((x) => {
+                    return x === undefined || x > 0;
+                });
+            });
+        });
+
+        it('should not infer when there is no movements', () =>{
+            let schedule = {
+                'location_records': [
+                    {},
+                    {},
+                    {},
+                    {},
+                    {}
+                ]
+            }
+
+            schedule = movementFormatting.performHeuristics(schedule);
+
+            schedule['location_records'].forEach((record) => {
+                expect(record).to.not.have.any.keys('timetable_variation', 'timetable_variation_prediction');
+            });
+        });
+
+        it('should not infer once a train has terminated', () =>{
+            let schedule = {
+                'location_records': [
+                    {
+                        'timetable_variation': 0
+                    },
+                    {
+                        'timetable_variation': 0
+                    },
+                    {
+                        'timetable_variation': 0
+                    },
+                    {
+                        'timetable_variation': 0
+                    },
+                    {
+                        'timetable_variation': 0
+                    }
+                ]
+            }
+
+            schedule = movementFormatting.performHeuristics(schedule);
+
+            schedule['location_records'].forEach((record) => {
+                expect(record).to.have.any.keys('timetable_variation');
+                expect(record).to.not.have.any.keys('timetable_variation_prediction');
+            });
         });
         
     });
