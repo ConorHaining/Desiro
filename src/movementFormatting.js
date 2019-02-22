1c846a972e0a23c361fa99c81e0f78b1b00c8baa
Conor Haining <conor.haining@gmail.com>
2019-02-22 13:02:40 +0000

    Tests & code to calculate the time

--- src/movementFormatting.js
+++ src/movementFormatting.js
@@ -1,3 +1,5 @@
+var { DateTime } = require('luxon');
+
 module.exports = {
     performHeuristics: (schedule) => {
         let locationRecords = schedule['location_records'];
@@ -7,7 +9,7 @@ module.exports = {
         locationRecords = locationRecords.map(record => {
             
             let timetableVariationArrival = (record['ARRIVAL'] == undefined) ?  undefined : record['ARRIVAL']['timetable_variation'];
-            let timetableVariationDeparture = (record['DEPARTURE'] == undefined) ?  undefined : record['ARRIVAL']['timetable_variation'];
+            let timetableVariationDeparture = (record['DEPARTURE'] == undefined) ?  undefined : record['DEPARTURE']['timetable_variation'];
 
             // console.log(`Arrival: ${timetableVariationArrival} | Prediction ${}`);
             // console.log(`Departure: ${} | Prediction ${}`);
@@ -33,10 +35,45 @@ module.exports = {
     },
 
     calculatePredictedTime: (schedule) => {
+        let locationRecords = schedule['location_records'];
+
+        locationRecords = locationRecords.map(record => {
+            let timetableVariationArrival = record['ARRIVAL']['timetable_variation_prediction'];
+            let timetableVariationDeparture = record['DEPARTURE']['timetable_variation_prediction'];
+
+            if (timetableVariationArrival !== undefined) {
+                let arrivalTime = module.exports.chopUpTime(record['arrival']);
+                arrivalTime = DateTime.fromObject(arrivalTime);
+                
+                let predictedArrival = arrivalTime.plus({minutes: timetableVariationArrival});
+                record['ARRIVAL']['predicted_arrival'] = predictedArrival.toLocaleString(DateTime.TIME_24_SIMPLE);
+            }
+            
+            if (timetableVariationDeparture !== undefined) {
+                let departureTime = module.exports.chopUpTime(record['departure']);
+                departureTime = DateTime.fromObject(departureTime);
+
+                let predictedDeparture = departureTime.plus({minutes: Math.max(0, timetableVariationDeparture)});
+                record['DEPARTURE']['predicted_departure'] = predictedDeparture.toLocaleString(DateTime.TIME_24_SIMPLE);
+            }
+
+            return record;
+        });
+
         return schedule;
     },
 
     createBasicMovementData: (schedule) => {
         
+    },
+
+    /**
+     * Helpers
+     */
+    chopUpTime: (time) => {
+        return {
+            hour: parseInt(time.substring(0, 2)),
+            minute: parseInt(time.substring(3, 5))
+        }
     }
 }
--- test/movementFormatting.test.js
+++ test/movementFormatting.test.js
@@ -224,7 +224,225 @@ describe('Movements', function() {
         
     });
 
-    
+    describe('Calculate time', function() {
+
+        const movementFormatting = require('../src/movementFormatting.js');
+
+        it('should calculate the correct time for on-time predictions', () =>{
+            let schedule = {
+                'location_records': [
+                    {
+                        'ARRIVAL' : {
+                            'timetable_variation': 0
+                        },
+                        'DEPARTURE' : {
+                            'timetable_variation': 0
+                        }
+                    },
+                    {
+                        'ARRIVAL' : {
+                            'timetable_variation': 0
+                        },
+                        'DEPARTURE' : {
+                            'timetable_variation': 0
+                        }
+                    },
+                    {
+                        'ARRIVAL' : {
+                            'timetable_variation': 0
+                        },
+                        'DEPARTURE' : {
+                            'timetable_variation': 0
+                        }
+                    },
+                    {
+                        'arrival': '12:00',
+                        'departure': '12:01',
+                        'ARRIVAL' : {
+                            'timetable_variation_prediction': 0
+                        },
+                        'DEPARTURE' : {
+                            'timetable_variation_prediction': 0
+                        }
+                    }
+                ]
+            };
+
+            schedule = movementFormatting.calculatePredictedTime(schedule);
+
+            schedule['location_records'].forEach((record) => {
+                expect(record['ARRIVAL']).to.satisfy((x) => {
+                    return (x['timetable_variation_prediction'] != undefined && x['predicted_arrival'] == '12:00')
+                        || (x['timetable_variation'] != undefined);
+                });
+                
+                expect(record['DEPARTURE']).to.satisfy((x) => {
+                    return (x['timetable_variation_prediction'] != undefined && x['predicted_departure'] == '12:01')
+                        || (x['timetable_variation'] != undefined);
+                });
+            });
+        });
+
+        it('should calculate the correct time for early predictions', () =>{
+            let schedule = {
+                'location_records': [
+                    {
+                        'ARRIVAL' : {
+                            'timetable_variation': 0
+                        },
+                        'DEPARTURE' : {
+                            'timetable_variation': 0
+                        }
+                    },
+                    {
+                        'ARRIVAL' : {
+                            'timetable_variation': 0
+                        },
+                        'DEPARTURE' : {
+                            'timetable_variation': 0
+                        }
+                    },
+                    {
+                        'ARRIVAL' : {
+                            'timetable_variation': 0
+                        },
+                        'DEPARTURE' : {
+                            'timetable_variation': 0
+                        }
+                    },
+                    {
+                        'arrival': '12:00',
+                        'departure': '12:01',
+                        'ARRIVAL' : {
+                            'timetable_variation_prediction': -1
+                        },
+                        'DEPARTURE' : {
+                            'timetable_variation_prediction': -1
+                        }
+                    }
+                ]
+            };
+
+            schedule = movementFormatting.calculatePredictedTime(schedule);
+
+            schedule['location_records'].forEach((record) => {
+                expect(record['ARRIVAL']).to.satisfy((x) => {
+                    return (x['timetable_variation_prediction'] != undefined && x['predicted_arrival'] == '11:59')
+                        || (x['timetable_variation'] != undefined);
+                });
+                
+                expect(record['DEPARTURE']).to.satisfy((x) => {
+                    return (x['timetable_variation_prediction'] != undefined && x['predicted_departure'] == '12:01')
+                        || (x['timetable_variation'] != undefined);
+                });
+            });
+        });
+
+        it('should calculate the correct time for late predictions', () =>{
+            let schedule = {
+                'location_records': [
+                    {
+                        'ARRIVAL' : {
+                            'timetable_variation': 0
+                        },
+                        'DEPARTURE' : {
+                            'timetable_variation': 0
+                        }
+                    },
+                    {
+                        'ARRIVAL' : {
+                            'timetable_variation': 0
+                        },
+                        'DEPARTURE' : {
+                            'timetable_variation': 0
+                        }
+                    },
+                    {
+                        'ARRIVAL' : {
+                            'timetable_variation': 0
+                        },
+                        'DEPARTURE' : {
+                            'timetable_variation': 0
+                        }
+                    },
+                    {
+                        'arrival': '12:00',
+                        'departure': '12:01',
+                        'ARRIVAL' : {
+                            'timetable_variation_prediction': 1
+                        },
+                        'DEPARTURE' : {
+                            'timetable_variation_prediction': 1
+                        }
+                    }
+                ]
+            };
+
+            schedule = movementFormatting.calculatePredictedTime(schedule);
+
+            schedule['location_records'].forEach((record) => {
+                expect(record['ARRIVAL']).to.satisfy((x) => {
+                    return (x['timetable_variation_prediction'] != undefined && x['predicted_arrival'] == '12:01')
+                        || (x['timetable_variation'] != undefined);
+                });
+                
+                expect(record['DEPARTURE']).to.satisfy((x) => {
+                    return (x['timetable_variation_prediction'] != undefined && x['predicted_departure'] == '12:02')
+                        || (x['timetable_variation'] != undefined);
+                });
+            });
+        });
+
+        it('should not calculate anything for no predictions', () =>{
+            let schedule = {
+                'location_records': [
+                    {
+                        'arrival': '12:00',
+                        'departure': '12:01',
+                        'ARRIVAL' : {
+                            'timetable_variation': 0
+                        },
+                        'DEPARTURE' : {
+                            'timetable_variation': 0
+                        }
+                    },
+                    {
+                        'arrival': '12:00',
+                        'departure': '12:01',
+                        'ARRIVAL' : {
+                            'timetable_variation': 0
+                        },
+                        'DEPARTURE' : {
+                            'timetable_variation': 0
+                        }
+                    },
+                    {
+                        'arrival': '12:00',
+                        'departure': '12:01',
+                        'ARRIVAL' : {
+                            'timetable_variation': 0
+                        },
+                        'DEPARTURE' : {
+                            'timetable_variation': 0
+                        }
+                    }
+                ]
+            };
+
+            schedule = movementFormatting.calculatePredictedTime(schedule);
+
+            schedule['location_records'].forEach((record) => {
+                expect(record['ARRIVAL']).to.satisfy((x) => {
+                    return x['predicted_arrival'] === undefined;
+                });
+                
+                expect(record['DEPARTURE']).to.satisfy((x) => {
+                    return x['predicted_departure'] === undefined;
+                });
+            });
+        });
+
+    });
 
     describe('Basic Details', function() {
         
