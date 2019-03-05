93417c32f511ea10c1ed18b7455bfd2f1a5b6045
Conor Haining <conor.haining@gmail.com>
2019-03-05 13:58:29 +0000

    Getting trainID from an array of Schedules

--- src/movementQuerying.js
+++ src/movementQuerying.js
@@ -1,6 +1,53 @@
+const ES = require('../inc/elasticsearch.js');
+
 module.exports = {
-    getTrainMovementId: (schedule) => {
-        
+    getTrainMovementIdFromSchedules: (schedules, when) => {
+        const date = when.toFormat('dd/LL/yyyy');
+        return new Promise((resolve, reject) => {
+          
+          schedules.map(schedule => {
+            const uid = schedule['uid'];
+            ES.search({
+              index: 'movement',
+              body: {
+                  "query": {
+                    "bool": {
+                      "must": [
+                        {
+                          "match": {
+                            "train_uid": uid
+                          }
+                        },
+                        {
+                          "range": {
+                            "received_at": {
+                              "format": "dd/MM/yyyy",
+                              "gte": date,
+                              "lte": date
+                            }
+                          }
+                        }
+                      ]
+                    }
+                  }
+                }
+              }).then(results => {
+                if(results.hits.total === 1){
+                    schedule['train_id'] = results.hits.hits[0]['_source']['train_id'];
+                }
+                
+                return schedule;
+              });
+          });
+
+          resolve(schedules);
+
+        });
+
+    },
+
+    getTrainMovementsFromSchedules: (schedules) => {
+
     },
 
     getTrainMovements: (schedule) => {
